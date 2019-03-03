d666c243a5a145c1293667c57971f19f52e53c51
Rob Bell <me@rob-bell.net>
2019-03-03 15:15:23 +0000

    Add TileArray helper - to be hooked up

---
+++ lib/TileArray.ts
@@ -0,0 +1,38 @@
+import { Tile, NullTile } from "./Tile";
+import { Coordinate } from "./Coordinate";
+
+export class TileArray {
+    tiles: Tile[][]
+
+    constructor(public _width: number, public height: number) {
+        this.tiles = []
+
+        for (var rowCount: number = 0; rowCount < height; rowCount++) {
+            this.tiles[rowCount] = []
+        }
+    }
+
+    getTileAt(coordinate: Coordinate): Tile {
+        return this.tiles[coordinate.x] && this.tiles[coordinate.x][coordinate.y] || new NullTile()
+    }
+
+    setTileAt(coordinate: Coordinate, tile: Tile): void {
+        this.tiles[coordinate.x][coordinate.y] = tile;
+    }
+
+    apply(action: (tile: Tile) => void): void {
+        this.tiles.forEach(row => {
+            row.forEach(tile => {
+                action(tile)
+            })
+        })
+    }
+
+    find(criteria: (tile: Tile) => boolean): Tile {
+        for (let rowCount = 0; rowCount < this.tiles.length; rowCount++) {
+            let matching = this.tiles[rowCount].filter(criteria)
+            if (matching.length > 0) return matching[0]
+        }
+        return new NullTile();
+    }
+}
